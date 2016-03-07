b98a5f1c93f517239c937622ce95b0223f68ae4e
Napon Taratan <napon@napontaratan.com>
2016-03-07 13:41:38 -0800

    Added interval update mechanism.

--- Node-Client/node.go
+++ Node-Client/node.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net"
 	"os"
+	"sync"
+	"time"
 )
 
 type GameState struct {
-	board Board
-	// where the players are now
-	currLocs map[string]Location
+	board    Board
+	currLocs map[string]Location // where the players are now
 	stat     GameStat
 }
 
@@ -23,15 +27,30 @@ type Board [][]string
 type Location struct {
 	x   int
 	y   int
-	dir int
+	dir string // 'U', 'D', 'L', 'R'
+}
+
+// Peers
+type Node struct {
+	id string
+	ip string
 }
 
 const (
 	boardSize int = 10
 )
 
-var gameState GameState
-var done chan int
+// Game variables.
+var nodeId string         // Name of client.
+var nodeAddr string       // IP of client.
+var msServerAddr string   // Matchmaking server IP.
+var httpServerAddr string // HTTP Server IP.
+var gameState GameState   // Overall GameState of the client.
+var nodes []Node          // All nodes in the game.
+
+// Sync variables.
+var waitGroup sync.WaitGroup // For internal processes.
+var done chan int            // For external processes.
 
 func main() {
 	if len(os.Args) < 5 {
@@ -43,10 +62,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	nodeId, nodeAddr, msServerAddr, httpServerAddr := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+	nodeId, nodeAddr, msServerAddr, httpServerAddr = os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+	init() // Initialize variables.
 
 	log.Println(nodeId, nodeAddr, msServerAddr, httpServerAddr)
-
 	initLogging()
 
 	httpMsg := make(chan string)
@@ -55,8 +74,15 @@ func main() {
 	go httpServe(httpMsg)
 	<-done
 	<-done
+
+	waitGroup.Add(1)    // Add an internal process.
+	go intervalUpdate() // Internal update mechanism.
+
+	// Wait until processes are done.
+	waitGroup.Wait()
 }
 
+// Initialize variables.
 func init() {
 	done = make(chan int, 2)
 	board := make([][]string, boardSize)
@@ -67,5 +93,43 @@ func init() {
 	scores := make(map[string]int)
 	gameStat := GameStat{scores}
 	gameState = GameState{board, currLocs, gameStat}
+	nodes = make([]Node, 0)
 	log.Println(gameState)
+}
+
+// Update peers with node's current location.
+func intervalUpdate() {
+	for {
+		currentLocationJSON, err := json.Marshal(gameState.currLocs[clientName])
+		checkErr(err)
+		for i, node := range nodes {
+			if node.id != nodeId {
+				sendUDPPacket(node.ip, currentLocationJSON)
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+// Send data to ip via UDP.
+func sendUDPPacket(ip string, data []byte) {
+	localAddr, err := net.ResolveUDPAddr("udp", nodeAddr)
+	checkErr(err)
+	ipAddr, err := net.ResolveUDPAddr("udp", ip)
+	checkErr(err)
+	udpConn, err := net.DialUDP("udp", localAddr, ipAddr)
+	checkErr(err)
+
+	defer udpConn.Close()
+
+	_, err = udpConn.Write(data)
+	checkErr(err)
+}
+
+// Error checking. Exit program when error occurs.
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 }
