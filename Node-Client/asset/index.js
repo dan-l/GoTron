58f07526b2bd8579cb5c18253219d2f3c2bad816
Alex Ng <ang04a@gmail.com>
2016-03-11 13:29:06 -0800

    Issue 24: Use Fabric.js as our framework/library for canvas drawing.
    Includes very basic input handling and message broadcasting as a starting point.

--- Node-Client/asset/index.js
+++ Node-Client/asset/index.js
@@ -1,3 +1,35 @@
 "use strict";
 
 var gSocket = io();
+
+var gCanvas = new fabric.Canvas("mainCanvas");
+var gPlayerRect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: "red",
+  width: 10,
+  height: 10
+});
+gCanvas.add(gPlayerRect);
+
+window.onkeydown = function(event) {
+  switch (event.key) {
+    case "w":
+      gPlayerRect.setTop(Math.max(gPlayerRect.getTop() - gPlayerRect.getHeight(), 0));
+      break;
+    case "a":
+      gPlayerRect.setLeft(Math.max(gPlayerRect.getLeft() - gPlayerRect.getHeight(), 0));
+      break;
+    case "s":
+      gPlayerRect.setTop(Math.min(gPlayerRect.getTop() + gPlayerRect.getHeight(), 200));
+      break;
+    case "d":
+      gPlayerRect.setLeft(Math.min(gPlayerRect.getLeft() + gPlayerRect.getWidth(), 200));
+      break;
+    default:
+      return;
+  }
+
+  gSocket.emit("playerMove", event.key);
+  gCanvas.renderAll();
+};
