b2bfe6961b518b592d5f6ad0c32b042fbd91bc5b
Alex Ng <ang04a@gmail.com>
2016-03-14 14:35:34 -0700

    Add initial support for setting up initial game config in the HTTP server and GUI layers.

--- Node-Client/asset/index.js
+++ Node-Client/asset/index.js
@@ -1,18 +1,27 @@
 "use strict";
 
-var gSocket = io();
+var PLAYER_RECT_WIDTH = 10;
+var PLAYER_RECT_HEIGHT = 10;
+var PLAYER_COLOURS = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "black",
+  "orange",
+];
 
+var gSocket = io();
 var gCanvas = new fabric.Canvas("mainCanvas");
-var gPlayerRect = new fabric.Rect({
-  left: 100,
-  top: 100,
-  fill: "red",
-  width: 10,
-  height: 10
-});
-gCanvas.add(gPlayerRect);
+var gPlayerRects = {};
+var gPlayerRect;
+var gInitialised = false;
 
 window.onkeydown = function(event) {
+  if (!gInitialised) {
+    return;
+  }
+
   switch (event.key) {
     case "w":
       gPlayerRect.setTop(Math.max(gPlayerRect.getTop() - gPlayerRect.getHeight(), 0));
@@ -36,3 +45,54 @@ window.onkeydown = function(event) {
 
   gCanvas.renderAll();
 };
+
+gSocket.on("config", function(msg) {
+  if (gInitialised) {
+    // TODO: Signal failure
+    return;
+  }
+
+  // As a reminder, we don't bother with security at all in this project.
+  var config;
+  try {
+    config = JSON.parse(msg);
+  } catch (e) {
+    // TODO: Signal failure
+    return;
+  }
+
+  var EXPECTED_CONFIG_PROPS = [
+    "players",
+    "selfID",
+  ];
+  for (var i = 0; i < EXPECTED_CONFIG_PROPS.length; i++) {
+    var expectedProp = EXPECTED_CONFIG_PROPS[i];
+    if (!(expectedProp in config)) {
+      console.log("missing prop: " + expectedProp);
+      // TODO: Signal failure
+      return;
+    }
+  }
+
+  if (config.players.length > PLAYER_COLOURS.length) {
+    console.log("player count too high");
+    // TODO: Signal failure
+    return;
+  }
+
+  for (var i = 0; i < config.players.length; i++) {
+    var rect = new fabric.Rect({
+      left: i * 20,
+      top: i * 20,
+      fill: PLAYER_COLOURS[i],
+      width: PLAYER_RECT_WIDTH,
+      height: PLAYER_RECT_HEIGHT,
+    });
+    gPlayerRects[config.players[i]] = rect;
+    gPlayerRect = gPlayerRects[config.selfID];
+    gCanvas.add(rect);
+  }
+
+  gCanvas.renderAll();
+  gInitialised = true;
+});
--- Node-Client/httpServer.go
+++ Node-Client/httpServer.go
@@ -16,6 +16,10 @@ func httpServe() {
 	server.On("connection", func(so socketio.Socket) {
 		log.Println("on connection")
 		so.Join("chat")
+		// TODO: Use JSON utilities to send the config instead of using error
+		//       prone manual strings.
+		so.Emit("config",
+			"{\"players\": [\"a\", \"b\", \"c\", \"d\"], \"selfID\": \"a\"}")
 		so.On("playerMove", func(direction string) {
 			log.Println("playerMove:", direction)
 		})
